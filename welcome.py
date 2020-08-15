c7c0cbcb4343f7c20b69a4a5dbd828d037b72004
Shubham <dev.shubh.am.dshah@gmail.com>
2020-08-16 00:34:45 +0530

    misc

--- welcome.py
+++ welcome.py
@@ -1,11 +1,12 @@
 import login_and_encryption 
+import getpass
 
 print("Welcome to FinTra λ - A CLI-Based Finance Tracker.")
 
 try:
     user_data_file = open("user_data.txt", 'r')
     username = input("Enter username: ")
-    password = getpass("Enter password: ")
+    password = getpass.getpass("Enter password: ")
     login_and_encryption.login(username, password)
 except FileNotFoundError:
     user_data_file = open("user_data.txt", 'w')
