295e31ad2b9b209de9295e9d5c9dc0e76fa99112
SirSnehGupta <guptasneh302004@gmail.com>
2020-08-16 01:20:42 +0530

    commit

--- fintra.py
+++ fintra.py
@@ -1,56 +1,76 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv_file_creation as cfc
+from getpass import  getpass
+import login_and_encryption as le
 
+user_data_file = open('user_data.txt','r')
 cfc.create_file()
-inc = pd.read_csv('income.csv', index_col=0)
-exp = pd.read_csv('outgo.csv', index_col=0)
+uname = passwd = None
 
-while True:
-    mnt = ['Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec','Jan','Feb','Mar']
-    cmd = input('Fintra λ ')
-    cmdl = cmd.split()
+def login():
+    global uname, passwd
+    print("Fintra Login")
+    uname = input("Enter your username: ")
+    passwd = getpass("Enter your password: ")
+login()
 
-    if cmdl[0] == 'income':
-        if cmdl[5] == '=':
-            inc.loc[cmdl[2],cmdl[4]] = int(cmdl[6])
-            print(inc)
-        elif cmdl[5] == '+':    
-            inc.loc[cmdl[2],cmdl[4]] += int(cmdl[6])
-            print(inc)
-    
-    elif cmdl[0] == 'expense':
-        if cmdl[5] == '=':
-            exp.loc[cmdl[2],cmdl[4]] = int(cmdl[6])
-            print(exp)
-        elif cmdl[5] == '+':    
-            exp.loc[cmdl[2],cmdl[4]] += int(cmdl[6])
-            print(exp)
+usr = user_data_file.readline()
+pwd = user_data_file.readline()
 
-    elif cmd == 'show':
-        print('Income Table:\n',inc)
-        print('Expense Table:\n',exp)
+if usr == (str(uname)+'\n') and pwd == (str(passwd)+'\n'):
+    print('\nWelcome',uname+'!')
+    inc = pd.read_csv('income.csv', index_col=0)
+    exp = pd.read_csv('outgo.csv', index_col=0)
 
-    elif cmd == 'inbar':
-        incs = input('Name any one income which you wanna observe in this bar chart: ')
-        plt.bar(mnt, list(inc[incs]), label = incs, width = 0.5)
-        plt.xlabel('Months')
-        plt.ylabel('Income Categories')
-        plt.legend()
-        plt.show()
+    while True:
+        mnt = ['Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec','Jan','Feb','Mar']
+        cmd = input('Fintra λ ')
+        cmdl = cmd.split()
 
-    elif cmd == 'exbar':
-        exps = input('Name any one expense which you wanna observe in this bar chart: ')
-        plt.bar(mnt, list(exp[exps]), label = exps, width = 0.5)
-        plt.xlabel('Months')
-        plt.ylabel('Expense Categories')
-        plt.legend()
-        plt.show() 
+        if cmdl[0] == 'income':
+            if cmdl[5] == '=':
+                inc.loc[cmdl[2],cmdl[4]] = int(cmdl[6])
+                print(inc)
+            elif cmdl[5] == '+':    
+                inc.loc[cmdl[2],cmdl[4]] += int(cmdl[6])
+                print(inc)
         
-    elif cmd == 'exit':
-        print('Saving and exiting... \nDone')
-        inc.to_csv('income.csv')
-        exp.to_csv('outgo.csv')
-        break       
-    else:
-        print("Invalid Input, please try again")
+        elif cmdl[0] == 'expense':
+            if cmdl[5] == '=':
+                exp.loc[cmdl[2],cmdl[4]] = int(cmdl[6])
+                print(exp)
+            elif cmdl[5] == '+':    
+                exp.loc[cmdl[2],cmdl[4]] += int(cmdl[6])
+                print(exp)
+
+        elif cmd == 'show':
+            print('Income Table:\n',inc)
+            print('Expense Table:\n',exp)
+
+        elif cmd == 'inbar':
+            incs = input('Name any one income which you wanna observe in this bar chart: ')
+            plt.bar(mnt, list(inc[incs]), label = incs, width = 0.5)
+            plt.xlabel('Months')
+            plt.ylabel('Income Categories')
+            plt.legend()
+            plt.show()
+
+        elif cmd == 'exbar':
+            exps = input('Name any one expense which you wanna observe in this bar chart: ')
+            plt.bar(mnt, list(exp[exps]), label = exps, width = 0.5)
+            plt.xlabel('Months')
+            plt.ylabel('Expense Categories')
+            plt.legend()
+            plt.show() 
+            
+        elif cmd == 'exit':
+            print('Saving and exiting... \nDone')
+            inc.to_csv('income.csv')
+            exp.to_csv('outgo.csv')
+            break       
+        else:
+            print("Invalid Input, please try again")
+
+else:
+    print('Authentication Failed')
--- login_and_encryption.py
+++ login_and_encryption.py
@@ -1,6 +1,34 @@
-import getpass
+from getpass import getpass
+
+uname = pwd = pwd2 = su = None
+def authenticate():
+    if pwd == pwd2:
+        global su
+        file = open('user_data.txt','a')
+        a = (str(uname)+'\n')
+        b = (str(pwd)+'\n')
+        file.write(str(a))
+        file.write(str(b))
+        su = True
+        print("Sign up Successful!\n")
+
+    else:
+        print("Password Mismatch detected, please try again.\n")
 
 def signup():
+    global uname, pwd, pwd2
+    uname = input("Enter your username: ")
+    pwd = getpass("Enter your password: ")
+    pwd2 = getpass("Confirm password: ")
+
+def run():
+    while True:
+        signup()
+        authenticate()
+        if su == True:
+            break
+
+""" def signup():
     user_data_file = open("user_data.txt", 'w')
     username = input("Please enter a username\nNOTE: Username and password cannot be changed.\nUsername: ")
     print("==========STORING==========")
@@ -13,13 +41,16 @@ def signup():
             password_entry1 = password_entry2 = ''
             password()
     password()
-    credentials = [username,password]
-    user_data_file.writelines(credentials)
+    #credentials = [username,password]
+    usr = (str(username)+'\n')
+    pwd = (str(password)+'\n')
+    user_data_file.write(usr)
+    user_data_file.write(pwd) """
 
 
-def login(username, password):
+""" def login(username, password):
     user_data_file = open("user_data.txt", 'r')
     if username == user_data_file.readline():
         print("==========AUTHORISING==========")
         if password == user_data_file.readline():
-            print("==========ACCESS GRANTED==========")
+            print("==========ACCESS GRANTED==========") """
